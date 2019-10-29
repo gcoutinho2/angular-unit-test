eaa59c68409ad48ae68ac454a14207a17cc717ae
Gabriel Coutinho <gabriel.resourceit.com>
2019-10-29 16:02:21 -0300

    forms test

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#TestApp
+TestApp
 
 Example project with unit tests.
--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,14 +1,32 @@
 import { TodoFormComponent } from './todo-form.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('TodoFormComponent', () => {
+  let component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should create a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
   });
 
-  it('', () => {
+  it('should make the name control required', () => {
+    const control = component.form.get('name');
+
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
   });
+
+  it('should make the email control not required', () => {
+    const control = component.form.get('email');
+
+    control.setValue('');
+
+    expect(control.valid).toBeTruthy();
+  });
+
 });
